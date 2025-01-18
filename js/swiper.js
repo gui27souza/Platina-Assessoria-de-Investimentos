bacb6ba0ad3cdf221972df077f25456e8143061e
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-01-18 13:43:17 -0300

    cambio

--- js/swiper.js
+++ js/swiper.js
@@ -10,3 +10,27 @@ var swiper = new Swiper(".swiper-investimentos", {
     }
 });
   
+var swiper = new Swiper(".swiper-cambio-1", {
+    effect: "slide",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 5269
+    }
+});
+  
+var swiper = new Swiper(".swiper-cambio-2", {
+    effect: "slide",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 2327
+    }
+});
+  
