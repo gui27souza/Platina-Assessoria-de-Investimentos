fe2c143e8dd89b4424e5ca84548bc7a083b6d2a6
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-01-18 13:42:46 -0300

    investimentos

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper-investimentos", {
+    effect: "slide",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 8376
+    }
+});
+  
