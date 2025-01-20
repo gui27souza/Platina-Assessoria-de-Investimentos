f8cbf9332d6946da0cb17787213294ec08eb3ff3
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-01-20 17:47:53 -0300

    scroll update

--- js/scroll.js
+++ js/scroll.js
@@ -2,12 +2,12 @@ const nav_wrapper = document.getElementById('nav-wrapper')
 
 let nav_height = nav_wrapper.offsetHeight
 
-function scrollNavTo(element, addtional_scroll=0) {
+function scrollNavTo(element) {
     const targetId = element.getAttribute('data-scroll')
     const targetElement = document.getElementById(targetId)
 
     const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY
-    const offsetPosition = elementPosition - nav_height - addtional_scroll
+    const offsetPosition = elementPosition - nav_height
 
     window.scrollTo({
         top: offsetPosition,
