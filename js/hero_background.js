aca7e97a119b606304df4d8dfe4a0a1ba5397cdf
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-01-20 17:52:16 -0300

    hero background update

--- js/hero_background.js
+++ js/hero_background.js
@@ -2,8 +2,8 @@ const main_section = document.getElementById('main-section')
 
 function heroBackgroundEffect() {
 
-    let min = -50000
-    let max = 50000
+    let min = -20000
+    let max = 20000
     let rand1 = Math.floor(Math.random() * (max - min + 1)) + min
     let rand2 = Math.floor(Math.random() * (max - min + 1)) + min
     
