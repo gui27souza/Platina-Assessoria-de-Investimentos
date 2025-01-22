1fb373cdb4519edd1ccd0ea6f952d56d4e40e9b9
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-01-22 15:14:29 -0300

    scroll nav fix

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,27 @@
-const nav_wrapper = document.getElementById('nav-wrapper')
+function scrollNav() {
 
-let nav_height = nav_wrapper.offsetHeight
-
-function scrollNavTo(element) {
-    const targetId = element.getAttribute('data-scroll')
-    const targetElement = document.getElementById(targetId)
-
-    const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY
-    const offsetPosition = elementPosition - nav_height
-
-    window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth',
-    })
+    const nav_wrapper = document.getElementById('nav-wrapper')
+    
+    let nav_height = nav_wrapper.offsetHeight
+    
+    function scrollNavTo(element) {
+        const targetId = element.getAttribute('data-scroll')
+        const targetElement = document.getElementById(targetId)
+        
+        const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY
+        const offsetPosition = elementPosition - nav_height
+        
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        })
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    scrollNav()
+})
+
+window.addEventListener("resize", () => {
+    scrollNav()
+})
